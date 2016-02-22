836497001204adbd2ab87043082fe4e8e14fbc74
brandon932 <bthornton_932@yahoo.com>
2016-02-21 22:41:34 -0700

    added buttons and sliders everything working

--- js/main.js
+++ js/main.js
@@ -1,5 +1,63 @@
 // add scripts
-
 $(document).on('ready', function() {
-  console.log('sanity check!');
+  $('#replay').hide();
+  $('#myvideo').load();
+  var video = document.getElementById("myvideo");
+  video.load();
+
+  $('#play').on("click", function(e){
+    e.preventDefault();
+    if (video.paused === true){
+      $('#play').html("pause");
+      video.play();
+    }else{
+      $('#play').html("play");
+      video.pause();
+    }
+  });
+
+  $('#replay').on("click", function(e){
+    e.preventDefault();
+    video.currentTime=0;
+    video.play();
+  });
+
+  $('#seek-bar').on("change", function(e){
+    var time = video.duration * ($('#seek-bar').val()/100);
+    video.currentTime= time;
+  });
+
+  $('#myvideo').on("timeupdate", function(e){
+    var time = (100/ video.duration)* video.currentTime;
+    $('#seek-bar').val(time);
+    if (video.duration === video.currentTime){
+      $('#replay').show();
+    }else{
+      $('#replay').hide();
+    }
+  });
+
+  $('#seek-bar').on("mousedown", function(e){
+    video.pause();
+  });
+
+  $('#seek-bar').on("mouseup", function(e){
+    video.play();
+  });
+
+  $("#volume-bar").on("change", function(e){
+    video.volume = $('#volume-bar').val();
+  });
+
+  $("#mute").on("click", function(e){
+    if (video.muted){
+      video.muted = false;
+      $("#mute").html("mute");
+    }else{
+      video.muted = true;
+      $("#mute").html("un-mute");
+    }
+  });
+
+
 });
